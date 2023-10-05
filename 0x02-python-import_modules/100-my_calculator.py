cc67ce02fd8b379b97f5c9411d690b40f3c81d62
Omar Tazi <omar.dah@outlook.com>
2023-10-05 16:06:04 -0700

    Updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    x = int(sys.argv[1])
-    y = int(sys.argv[3])
-    print("{} {} {} = {}".format(x, sys.argv[2], y, ops[sys.argv[2]](x, y)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 
 
-def magic_calculation(x, y):
+def magic_calculation(a, b):
     """Match bytecode provided by Holberton School."""
     from magic_calculation_102 import add, sub
 
-    if x < y:
+    if a < b:
         c = add(x, y)
         for i in range(4, 6):
-            z = add(z, i)
-        return (z)
+            c = add(c, i)
+        return (c)
 
     else:
-        return(sub(x, y)
+        return(sub(a, b)
